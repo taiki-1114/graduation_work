6e54aab260f1ee98d5035c370709c26a68f73a5a
taiki-1114 <kame.taiki@gmail.com>
2024-09-23 20:59:25 +0900

    いいね機能の非同期化に成功

--- app/controllers/article_likes_controller.rb
+++ app/controllers/article_likes_controller.rb
@@ -2,11 +2,15 @@ class ArticleLikesController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     current_user.like(@article)
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html{ redirect_to @article}
+    end
   end
 
   def destroy
     @article = current_user.article_likes.find(params[:id]).article
     current_user.unlike(@article)
   end
-
 end
