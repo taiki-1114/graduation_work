27e8e70319a35807c4aa4044ad6311019e6070ea
taiki-1114 <kame.taiki@gmail.com>
2026-03-07 15:39:17 +0900

    いいねコントローラーのメモの追加

--- app/controllers/article_likes_controller.rb
+++ app/controllers/article_likes_controller.rb
@@ -1,5 +1,6 @@
 class ArticleLikesController < ApplicationController
   #投稿のいいねの作成、削除を制御するコントローラー
+  #投稿のいいねを作成するコントローラー
   def create
     @article = Article.find(params[:article_id])
     current_user.like(@article)
@@ -9,7 +10,7 @@ class ArticleLikesController < ApplicationController
       format.html{ redirect_to @article}
     end
   end
-
+#投稿のいいねの削除するコントローラー
   def destroy
     @article = current_user.article_likes.find(params[:id]).article
     current_user.unlike(@article)
